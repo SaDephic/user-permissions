71dbb42a9a46f967360ef15d8dde10f497bcb688
SaDephic <ortega.mafan@gmail.com>
2024-02-09 14:53:49 +0600

    rewrite

--- user-permissions/Program.cs
+++ user-permissions/Program.cs
@@ -1,48 +1,57 @@
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 using System;
-using System.Threading.Tasks;
+
 using user_permissions.Data;
 using user_permissions.Permission;
 using user_permissions.Seeds;
 
-namespace user_permissions
+var builder = WebApplication.CreateBuilder(args);
+//add Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//add DB Services and Configurations
+builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(System.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
 {
-    public class Program
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    var logger = loggerFactory.CreateLogger("app");
+    try
     {
-        public async static Task Main(string[] args)
-        {
-            var host = WebApplication.CreateBuilder(args);
-
-            host.Services.AddControllers();
-
-            services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-
-            using (var scope = host.Build().Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                var logger = loggerFactory.CreateLogger("app");
-                try
-                {
-                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    await Seeds.DefaultUsers.SeedSuperAdminAsync(userManager, roleManager);
-                    logger.LogInformation("Application Starting");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogWarning(ex, "An error occurred seeding the DB");
-                }
-            }
-        }
+        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await Seeds.DefaultUsers.SeedSuperAdminAsync(userManager, roleManager);
+        logger.LogInformation("Application Starting");
+    }
+    catch (Exception ex)
+    {
+        logger.LogWarning(ex, "An error occurred seeding the DB");
     }
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+
