6de8d0dec447bf0bec60a34a8eac7c410ac081c8
SaDephic <ortega.mafan@gmail.com>
2024-02-09 23:10:59 +0600

    build docker-compose

---
+++ user-permissions/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using IO.Swagger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace user_permissions
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
--- user-permissions/Program.cs
+++ user-permissions/Program.cs
@@ -1,15 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-
-using Microsoft.Extensions.DependencyInjection;
-
-using System;
-
-using user_permissions.Data;
-using user_permissions.Permission;
-using user_permissions.Seeds;
+using user_permissions;
 
 var builder = WebApplication.CreateBuilder(args);
 //add Services
@@ -17,41 +9,64 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//add DB Services and Configurations
-builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(System.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllers();
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connection));
 
 var app = builder.Build();
 
-
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-    var logger = loggerFactory.CreateLogger("app");
-    try
-    {
-        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-        await Seeds.DefaultUsers.SeedSuperAdminAsync(userManager, roleManager);
-        logger.LogInformation("Application Starting");
-    }
-    catch (Exception ex)
-    {
-        logger.LogWarning(ex, "An error occurred seeding the DB");
-    }
+
 }
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+/*namespace user_permissions.Constant
+{
+    public static class Permissions
+    {
+        public static List<string> GeneratePermissionsForModule(string module)
+        {
+            return new List<string>()
+            {
+                $"Permissions.{module}.Add",
+                $"Permissions.{module}.Read",
+                $"Permissions.{module}.Manager_Read",
+                $"Permissions.{module}.HealthCheck_Read",
+                $"Permissions.{module}.HealthCheck_Manager_Read",
+            };
+        }
+
+        public static class Products
+        {
+            public const string Add = "PERM_USER_ROLE_ADD";
+            public const string Read = "PERM_ABSENCE_READ";
+            public const string Manager_Read = "PERM_ABSENCE_MANAGER_READ";
+            public const string HealthCheck_Add = "PERM_HEALTHCHECK_ADD";
+            public const string HealthCheck_Manager_Read = "PERM_HEALTHCHECK_MANAGER_READ";
+        }
+    }
+}
+
+namespace user_permissions.Constant
+{
+    public enum Roles
+    {
+        EMPLOYEE,
+        MANAGER,
+        HRPARTNER,
+        HRDEV,
+        SUPERUSER
+    }
+}*/
 
 
